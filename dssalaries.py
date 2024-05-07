c128b868d1635ed3262f0ad16ff644349f0d1c94
Annisadpk <127417498+Annisadpk@users.noreply.github.com>
2024-05-07 13:10:04 +0700

    Update dssalaries.py

--- dssalaries.py
+++ dssalaries.py
@@ -5,32 +5,29 @@ import seaborn as sns
 
 # Load data
 @st.cache
-def load_data(file_path):
-    data = pd.read_csv(file_path)
+def load_data():
+    data = pd.read_csv("ds_salaries.csv")
     return data
 
-# Sidebar for file upload
-uploaded_file = st.sidebar.file_uploader("Upload CSV", type=['csv'])
+# Load data
+data = load_data()
 
-if uploaded_file is not None:
-    data = load_data(uploaded_file)
+# Top 5 positions with highest salaries
+st.subheader("5 Posisi dengan Gaji Terbesar")
+top_5_positions = data.groupby('job_title')['salary_in_usd'].mean().nlargest(5)
+st.bar_chart(top_5_positions)
 
-    # Top 5 positions with highest salaries
-    st.subheader("5 Posisi dengan Gaji Terbesar")
-    top_5_positions = data.groupby('job_title')['salary_in_usd'].mean().nlargest(5)
-    st.bar_chart(top_5_positions)
+# Distribution of job positions
+st.subheader("10 Posisi Pekerjaan Teratas")
+position_distribution = data['job_title'].value_counts().head(10)
+st.write(position_distribution)
 
-    # Distribution of job positions
-    st.subheader("10 Posisi Pekerjaan Teratas")
-    position_distribution = data['job_title'].value_counts().head(10)
-    st.write(position_distribution)
-
-    # Scatter plot of salary against experience level
-    st.subheader("Korelasi antara Gaji dan Tingkat Pengalaman")
-    sns.set_style("whitegrid")
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(data=data, x='experience_level', y='salary_in_usd', alpha=0.5)
-    plt.title('Korelasi antara Gaji dan Tingkat Pengalaman')
-    plt.xlabel('Tingkat Pengalaman')
-    plt.ylabel('Gaji (USD)')
-    st.pyplot(plt)
+# Scatter plot of salary against experience level
+st.subheader("Korelasi antara Gaji dan Tingkat Pengalaman")
+sns.set_style("whitegrid")
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='experience_level', y='salary_in_usd', alpha=0.5)
+plt.title('Korelasi antara Gaji dan Tingkat Pengalaman')
+plt.xlabel('Tingkat Pengalaman')
+plt.ylabel('Gaji (USD)')
+st.pyplot(plt)
