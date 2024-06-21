4bc41a1729c1de84e158fb020b43f070b8dcedf9
Annisadpk <127417498+Annisadpk@users.noreply.github.com>
2024-06-22 06:07:02 +0700

    Update dssalaries.py

--- dssalaries.py
+++ dssalaries.py
@@ -14,8 +14,8 @@ data = load_data()
 
 # Top 10 Film berdasarkan IMDb Rating
 st.header('Top 10 Film dengan IMDb Rating Tertinggi')
-top_10_movies = data.sort_values(by='IMDb Rating', ascending=False).head(10))
-plt.barh(top_10_movies['Title'], top_10_movies['IMDb Rating'], color='chocolate')
+top_10_movies = data.sort_valuesby='salary_in_usd', ascending=False.head(10)
+plt.barh(top_10_movies['job_title'], top_10_movies['salary_in_usd'], color='chocolate')
 st.bar_chart("Top 10 Film dengan IMDb Rating Tertinggi")
 st.write("Grafik ini menampilkan 10 film dengan rating IMDb tertinggi dalam data. Film-film ini memiliki rating yang sangat tinggi, menunjukkan bahwa mereka sangat dihargai oleh penonton dan kritikus.")
 
